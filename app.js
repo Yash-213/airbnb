9eec2bce8d96dd9f04aa34dbba52668f64f3f57f
Yash Waychal <yash.waychal02@gmail.com>
2025-10-05 00:20:05 +0530

    Remove Error handlers

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ const Listing = require("./models/listing");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
-const ExpressError = require("./utils/ExpressError");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
 
 main()
     .then(() => {
-        console.log('Connected!');
+        console.log('Connected to MongoDB!');
     })
     .catch((err) => {
         console.log(err);
@@ -22,84 +21,70 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
+// ---------- Setup ----------
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate)
 
+// ---------- Routes ----------
+
+// Home
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
+
 // Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
     const allListing = await Listing.find({});
-    res.render("listings/index", { allListing }); 
+    res.render("listings/index", { allListing });
 }));
 
-//New Route
-app.get("/listings/new", (req,res)=>{
+// New Route
+app.get("/listings/new", (req, res) => {
     res.render("listings/new");
 });
 
-//Create Listing
-app.post("/listings", wrapAsync(async(req, res, next)=>{
+// Create Listing
+app.post("/listings", wrapAsync(async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
-    res.redirect("/listings")
+    res.redirect("/listings");
 }));
 
-//Show Data
-app.get("/listings/:id", wrapAsync(async(req, res, next)=>{
-    let {id} = req.params;
+// Show Data
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    if (!listing) {
-        return next(new ExpressError(404, "Listing not found!"));
-    }
-    res.render("listings/show",{listing});
+    res.render("listings/show", { listing });
 }));
 
-//Update Data
-app.get("/listings/:id/edit", wrapAsync(async(req, res, next)=>{
-    let {id} = req.params;
+// Edit Form
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    if (!listing) {
-        return next(new ExpressError(404, "Listing not found!"));
-    }
-    res.render("listings/edit", {listing});
+    res.render("listings/edit", { listing });
 }));
 
-app.put("/listings/:id", wrapAsync(async(req,res, next)=>{
-    let {id} = req.params;
-    const listing = await Listing.findByIdAndUpdate(id,{...req.body.listing});
-    if (!listing) {
-        return next(new ExpressError(404, "Listing not found!"));
-    }
+// Update Listing
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
 }));
 
-//Delete Route
-app.delete("/listings/:id", wrapAsync(async(req, res, next)=>{
-    let {id} = req.params;
+// Delete Route
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const deletedListing = await Listing.findByIdAndDelete(id);
-    if (!deletedListing) {
-        return next(new ExpressError(404, "Listing not found!"));
-    }
     console.log(deletedListing);
     res.redirect("/listings");
 }));
 
-app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page Not Found!"));
-});
-
-app.use((err, req, res, next) => {
-    let { statusCode = 500, message = "Something went wrong!" } = err;
-    res.status(statusCode).render("listings/error", { message });
-});
-
+// ---------- Server ----------
 const port = 5000;
 app.listen(port, () => {
-    console.log("Connected to port 5000...");
+    console.log(`Connected to port ${port}...`);
 });
--- init/index.js
+++ init/index.js
@@ -1,23 +1,25 @@
 const mongoose = require("mongoose");
-const Listing = require("../models/listing");
-const initData = require("./data");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 async function main() {
-    try {
-        await mongoose.connect(MONGO_URL);
-        console.log("MongoDB connected!");
-
-        await Listing.deleteMany({});
-        await Listing.insertMany(initData.data);
-        console.log("Data initialized.");
-
-        await mongoose.disconnect(); 
-        console.log("MongoDB disconnected.");
-    } catch (err) {
-        console.error("Error during DB initialization:", err);
-    }
+  await mongoose.connect(MONGO_URL);
 }
 
-main();
+const initDB = async () => {
+  await Listing.deleteMany({});
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+};
+
+initDB();
