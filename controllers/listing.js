de0a04064d4332e133111e712f846e1ace385d84
Yash Waychal <yash.waychal02@gmail.com>
2025-12-22 22:38:50 +0530

    add update image feature in edit form

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,56 +1,68 @@
 const Listing = require("../models/listing");
 
 module.exports.index = async (req, res) => {
-    const allListing = await Listing.find({});
-    res.render("listings/index", { allListing });
+  const allListing = await Listing.find({});
+  res.render("listings/index", { allListing });
 };
 
 module.exports.rederNewForm = (req, res) => {
-    res.render("listings/new");
+  res.render("listings/new");
 };
 
 module.exports.showListing = async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id).populate({path :"reviews", populate: {path: "author"}}).populate("owner");
-    if(!listing){
-        req.flash("error", "Listing Does not exist!!");
-        return res.redirect("/listings");
-    }
-    res.render("listings/show", { listing });
+  let { id } = req.params;
+  const listing = await Listing.findById(id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("owner");
+  if (!listing) {
+    req.flash("error", "Listing Does not exist!!");
+    return res.redirect("/listings");
+  }
+  res.render("listings/show", { listing });
 };
 
 module.exports.createListing = async (req, res) => {
-    let url = req.file.path;
-    let filename = req.file.filename;
-    const newListing = new Listing(req.body.listing);
-    newListing.owner = req.user._id;
-    newListing.image = { filename, url };
-    await newListing.save();
-    req.flash("seccess", "New Listing created!!");
-    res.redirect("/listings");
+  let url = req.file.path;
+  let filename = req.file.filename;
+  const newListing = new Listing(req.body.listing);
+  newListing.owner = req.user._id;
+  newListing.image = { filename, url };
+  await newListing.save();
+  req.flash("seccess", "New Listing created!!");
+  res.redirect("/listings");
 };
 
 module.exports.editListing = async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    if(!listing){
-        req.flash("error", "Listing Does not exist!!");
-        return res.redirect("/listings");
-    }
-    res.render("listings/edit", { listing });
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    req.flash("error", "Listing Does not exist!!");
+    return res.redirect("/listings");
+  }
+
+  let originalImageUrl = listing.image.url;
+  originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_200,w_250");
+  console.log(listing.image.url);
+  res.render("listings/edit", { listing, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    req.flash("seccess", "Listing Updated!!");
-    res.redirect(`/listings/${id}`);
+  let { id } = req.params;
+  let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  if (typeof req.file !== "undefined") {
+    let url = req.file.path;
+    let filename = req.file.filename;
+    listing.image = { filename, url };
+    await listing.save();
+  }
+  req.flash("seccess", "Listing Updated!!");
+  res.redirect(`/listings/${id}`);
 };
 
 module.exports.destroyListing = async (req, res) => {
-    let { id } = req.params;
-    const deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    req.flash("seccess", "Listing Deleted!!");
-    res.redirect("/listings");
+  let { id } = req.params;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  req.flash("seccess", "Listing Deleted!!");
+  res.redirect("/listings");
 };
--- routes/listing.js
+++ routes/listing.js
@@ -30,6 +30,8 @@ router
   .put(
     isLoggedIn,
     isOwner,
+    upload.single("listing[image][url]"),
+    cloudUpload,
     validateListing,
     wrapAsync(listingController.updateListing)
   )
